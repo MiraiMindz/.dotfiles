e61d409371bd9f00cc46d586cb6d1028fbadbc30
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2022-01-12 17:15:03 -0300

    edited `installConfigs.sh`

--- installConfigs.sh
+++ installConfigs.sh
@@ -36,11 +36,21 @@ TEXTSTRIKE_OFF='\e[29m'
 NOCOLOR='\e[39m'
 TEXTRESETALL='\e[m'
 
+BACKUPFOLDER=$HOME/old_config_backup
+DOTFILESDIR=$HOME/.dotfiles
+
 ### Functions
+
+mkbkp() {
+    mkdir -v -p $1
+    mv -v $2/* $1/
+    printf "Backup done, proceeding with Installation.\n"
+}
+
 initgit() {
     if [[ -e $(which gh) ]]; then
         printf "Github CLI is installed: $(which gh)\n"
-        cd $HOME/.dotfiles/
+        cd $DOTFILESDIR/
         git init -b main
         git add .
         git commit -m "created .dotfiles repo from script"
@@ -54,7 +64,7 @@ initgit() {
         answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
         stty $old_stty_cfg
         if echo "$answer" | grep -iq "^y" ;then
-            cd $HOME/.dotfiles/
+            cd $DOTFILESDIR/
             git init -b main
             git add .
             git commit -m "created .dotfiles repo from script"
@@ -68,45 +78,59 @@ initgit() {
     fi
 }
 
-mkbkp() {
-    printf "$1\n"
-    mv -v $2 $3
-    ln -v -sf $4 $5
-}
-
-mkinst() {
-    mkdir -v -p $1
-    mkdir -v -p $2
-    cp -v -r $3 $2
-    ln -v -sf $2 $1
-}
-
 createDotfiles_BackupDir() {
-    if [[ -d $HOME/old_config_backup ]]; then
-        printf "${DARK_BLUE}old_config_backup${NOCOLOR} Directory Already Exists\n"
+    if [[ -d $BACKUPFOLDER ]]; then
+        printf "${DARK_BLUE}$BACKUPFOLDER${NOCOLOR} Directory Already Exists\n"
+        mkdir $HOME/old_config_backup2
+        mv -v BACKUPFOLDER/* $HOME/old_config_backup2
     else
-        printf "Creating ${DARK_BLUE}old_config_backup${NOCOLOR} Directory\n"
-        mkdir $HOME/old_config_backup
+        printf "Creating ${DARK_BLUE}$BACKUPFOLDER${NOCOLOR} Directory\n"
+        mkdir $BACKUPFOLDER/
     fi
     if [[ -d $HOME/.dotfiles ]];  then
-        printf "${DARK_BLUE}.dotfiles${NOCOLOR} Directory Already Exists\n"
+        printf "${DARK_BLUE}$DOTFILESDIR${NOCOLOR} Directory Already Exists\n"
         printf "Backing up\n"
-        mv $HOME/.dotfiles/* $HOME/old_config_backup/
+        mv -v $DOTFILESDIR/* $BACKUPFOLDER/
+        mv -v * $DOTFILESDIR/
     else
-        printf "Creating ${DARK_BLUE}.dotfiles${NOCOLOR} Directory\n"
+        printf "Creating ${DARK_BLUE}$DOTFILESDIR${NOCOLOR} Directory\n"
+        mkdir $DOTFILESDIR
+        mv -v * $DOTFILESDIR/
     fi
 }
 
 installI3WM() {
-    printf "${DARK_GREEN}INSTALLING${NOCOLOR} I3wm Config\n"
+    if [[ -e $(which i3) ]]; then
+        printf "${DARK_GREEN}INSTALLING${NOCOLOR} I3wm Config\n"
+        if [[ -d $($HOME/.config/i3) ]]; then
+            printf "i3 config found, creating backup in $BACKUPFOLDER/\n"
+            mkbkp $BACKUPFOLDER/i3wm $HOME/.config/i3
+            ln -v -sf $HOME/.dotfiles/environment/i3wm/* $HOME/.config/i3/
+        else
+            ln -v -sf $HOME/.dotfiles/environment/i3wm/* $HOME/.config/i3/
+        fi
+    fi
 }
 
 installPicom() {
-    printf "${DARK_GREEN}INSTALLING${NOCOLOR} Picom Config\n"
+    if [[ -e $(which picom) ]]; then
+        printf "${DARK_GREEN}INSTALLING${NOCOLOR} Picom Config\n"
+        if [[ -d $($HOME/.config/picom) ]]; then
+            printf "picom config found, creating backup in $BACKUPFOLDER/\n"
+            mkbkp $BACKUPFOLDER/picom $HOME/.config/picom
+            ln -v -sf $HOME/.dotfiles/environment/picom/* $HOME/.config/picom/
+        else
+            ln -v -sf $HOME/.dotfiles/environment/picom/* $HOME/.config/picom/
+        fi
+    fi
 }
 
 installPolybar() {
     printf "${DARK_GREEN}INSTALLING${NOCOLOR} Polybar Config\n"
+}
+
+installTerminator() {
+    printf "${DARK_GREEN}INSTALLING${NOCOLOR} Terminator Config\n"
 }
 
 installTerminator() {
@@ -133,6 +157,21 @@ installRofiApplets() {
     printf "${DARK_GREEN}INSTALLING${NOCOLOR} Rofi Applets Config\n"
 }
 
+installSDDMTheme() {
+    if [[ -e $(which sddm) ]]; then
+        if [[ -d /usr/share/sddm/themes/Sugar-Candy ]]; then
+            printf "${DARK_GREEN}INSTALLING${NOCOLOR} SDDM Sugar Candy Theme Config\n"
+        else
+            printf "Install the Sugar Candy Theme ${DARK_BLUE} (https://github.com/Kangie/sddm-sugar-candy)${NOCOLOR}\n"
+            printf "And run the following commands\n"
+            printf "${DARK_YELLOW}sudo mv -v \$HOME/.dotfiles/environment/sddm/Themes/Wallpapers/* /usr/share/sddm/themes/Sugar-Candy/Backgrounds/${NOCOLOR}"
+            printf "${DARK_YELLOW}sudo ln -sf /usr/share/sddm/themes/Sugar-Candy/* \$HOME/.dotfiles/environment/sddm/Themes/SugarCandy/${NOCOLOR}"
+            printf "${DARK_YELLOW}sudo mkdir /etc/sddm.conf.d${NOCOLOR}\n"
+            printf "${DARK_YELLOW}sudo ln -sf \$HOME/.dotfiles/environment/sddm/config.conf /etc/sddm.conf.d/${NOCOLOR}\n"
+        fi
+    fi
+}
+
 checkPackages() {
     _checkPkgs() {
         if [[ -e $(which $1) ]]; then
@@ -145,6 +184,7 @@ checkPackages() {
     _checkPkgs "picom"
     _checkPkgs "polybar"
     _checkPkgs "terminator"
+    -_checkPkgs "cool-retro-term"
     _checkPkgs "neofetch"
     _checkPkgs "vim"
     _checkPkgs "rofi"
@@ -152,6 +192,12 @@ checkPackages() {
         printf "Rofi Applets is ${DARK_GREEN}INSTALLED${NOCOLOR}\n"
     else
         printf "Rofi Applets is ${DARK_RED}MISSING${NOCOLOR}\n"
+    fi
+    _checkPkgs "sddm"
+    if [[ -d /usr/share/sddm/themes/Sugar-Candy ]]; then
+        printf "SDDM Sugar Candy Theme is ${DARK_GREEN}INSTALLED${NOCOLOR}\n"
+    else
+        printf "SDDM Sugar Candy Theme is ${DARK_RED}MISSING${NOCOLOR}\n"
     fi
 }
 
@@ -186,9 +232,9 @@ printf "${TEXTBOLD_ON}${DARK_RED}%*s${TEXTRESETALL}\n" $(((${#ln1}+$COLUMNS)/2))
 printf "${TEXTBOLD_ON}${DARK_RED}%*s${TEXTRESETALL}\n" $(((${#ln2}+$COLUMNS)/2)) "$ln2"
 printf %"$COLUMNS"s |tr " " "="
 
-printf "${DARK_GREEN}Picom ${DARK_PURPLE}[Ibhagwan Fork]${DARK_BLUE}(https://github.com/ibhagwan/picom)\n${DARK_GREEN}i3wm${DARK_BLUE} (https://github.com/Airblader/i3)\n${DARK_GREEN}Neofetch${DARK_BLUE} (https://github.com/dylanaraps/neofetch)\n${DARK_GREEN}Polybar${DARK_BLUE} (https://github.com/polybar/polybar)\n${DARK_GREEN}Rofi${DARK_BLUE} (https://github.com/davatorium/rofi)\n${DARK_GREEN}Rofi Applets${DARK_BLUE} (https://github.com/adi1090x/rofi)\n${DARK_GREEN}Terminator${DARK_BLUE} (https://github.com/gnome-terminator/terminator)\n${DARK_GREEN}Vim${DARK_BLUE} (https://github.com/vim/vim)\n"
+printf "${DARK_GREEN}Picom ${DARK_PURPLE}[Ibhagwan Fork]${DARK_BLUE}(https://github.com/ibhagwan/picom)\n${DARK_GREEN}i3wm${DARK_BLUE} (https://github.com/Airblader/i3)\n${DARK_GREEN}Neofetch${DARK_BLUE} (https://github.com/dylanaraps/neofetch)\n${DARK_GREEN}Polybar${DARK_BLUE} (https://github.com/polybar/polybar)\n${DARK_GREEN}Rofi${DARK_BLUE} (https://github.com/davatorium/rofi)\n${DARK_GREEN}Rofi Applets${DARK_BLUE} (https://github.com/adi1090x/rofi)\n${DARK_GREEN}Terminator${DARK_BLUE} (https://github.com/gnome-terminator/terminator)\n${DARK_GREEN}Cool Retro Term${DARK_BLUE} (https://github.com/Swordfish90/cool-retro-term)\n${DARK_GREEN}Vim${DARK_BLUE} (https://github.com/vim/vim)\n${DARK_GREEN}SDDM ${DARK_BLUE}(https://github.com/sddm/sddm)\n${DARK_GREEN}SDDM Sugar Candy${DARK_BLUE} (https://github.com/Kangie/sddm-sugar-candy)${NOCOLOR}\n"
 
-printf "${DARK_YELLOW}THIS SCRIPT WILL CREATE A FOLDER INSIDE THE ${DARK_CYAN}\$HOME ${DARK_BLUE}($HOME)${DARK_YELLOW} DIRECTIORY CALLED ${DARK_CYAN}.dotfiles${DARK_YELLOW}\nAND THEN WILL CREATE SYMLINKS FROM THAT DIRECTORY TO THE ORIGINAL CONFIG FILES.\nIF ANY CONFIGURATION IS FOUND IT WILL BE BACKED UP IN A CREATED FOLDER ${DARK_BLUE}($HOME/old_config_backup)${DARK_YELLOW}\nTHIS SCRIPT USES THE BOURNE AGAIN SHELL (BASH) AS MAIN USER SHELL\nIF YOU USE THE Z SHELL (ZSH) PLEASE DENY THE SHELL IMPORTS WHEN PROMPTED${TEXTRESETALL}\n"
+printf "${DARK_YELLOW}THIS SCRIPT WILL CREATE A FOLDER INSIDE THE ${DARK_CYAN}\$HOME ${DARK_BLUE}($HOME)${DARK_YELLOW} DIRECTIORY CALLED ${DARK_CYAN}.dotfiles${DARK_YELLOW}\nAND THEN WILL CREATE SYMLINKS FROM THAT DIRECTORY TO THE ORIGINAL CONFIG FILES.\nIF ANY CONFIGURATION IS FOUND IT WILL BE BACKED UP IN A CREATED FOLDER ${DARK_BLUE}($BACKUPFOLDER/)${DARK_YELLOW}\nTHIS SCRIPT USES THE BOURNE AGAIN SHELL (BASH) AS MAIN USER SHELL\nIF YOU USE THE Z SHELL (ZSH) PLEASE DENY THE SHELL IMPORTS WHEN PROMPTED${TEXTRESETALL}\n"
 
 echo -e -n "Do you have these packages installed (${DARK_GREEN}y${NOCOLOR}/${DARK_RED}n${NOCOLOR})? "
 old_stty_cfg=$(stty -g)
