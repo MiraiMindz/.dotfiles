3f953e4f2bcdc61c625bbf5272c572a486bde369
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2022-01-27 00:50:04 -0300

    added Ctrl+Arrow navigations

--- shells/zsh/zsh_keybinds.sh
+++ shells/zsh/zsh_keybinds.sh
@@ -38,3 +38,7 @@ if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
     add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
     add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
 fi
+
+# Ctrl + ArrowL/R Navigation
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
