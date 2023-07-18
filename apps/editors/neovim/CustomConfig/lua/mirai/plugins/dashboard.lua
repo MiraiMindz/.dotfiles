24a0adf2908508c0aadbba30968806213dd155a3
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2023-07-18 19:17:55 -0300

    finished dashboard

--- apps/editors/neovim/CustomConfig/lua/mirai/plugins/dashboard.lua
+++ apps/editors/neovim/CustomConfig/lua/mirai/plugins/dashboard.lua
@@ -1,3 +1,21 @@
-local M = {}
+local M = {
+    theme = "hyper",
+    shortcut_type = "number",
+    config = {
+        shortcut = {
+            { desc = '', group = '@property', action = '', key = 'MiraiMindz' },
+        },
+        footer = { "", "Mirai" },
+        packages = { enable = true },
+        project = { enable = true, limit = 4 },
+        mru = { limit = 4 },
+    },
+    preview = {
+        command = "sh",
+        file_path = "$DOTFILES/apps/editors/neovim/CustomConfig/lua/mirai/pluginsAssets/dashboard_art.sh",
+        file_height = 27,
+        file_width = 48,
+    }
+}
 
 return M
