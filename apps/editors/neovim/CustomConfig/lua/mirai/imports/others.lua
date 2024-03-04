eb9ae057d0580e9fbf5df75bb5a1a8d82e537bc0
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-03-04 17:32:50 -0300

    made some changes

--- apps/editors/neovim/CustomConfig/lua/mirai/imports/others.lua
+++ apps/editors/neovim/CustomConfig/lua/mirai/imports/others.lua
@@ -13,33 +13,89 @@ local M = {
         "epwalsh/obsidian.nvim",
         version = "*", -- recommended, use latest release instead of latest commit
         lazy = true,
-        ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-        --   "BufReadPre path/to/my-vault/**.md",
-        --   "BufNewFile path/to/my-vault/**.md",
-        -- },
+        -- ft = "markdown",
+        event = {
+            "BufReadPre " .. vim.fn.expand("~") .. "/usr/Documents/Obsidian/Vaults/Notes/**/*.md",
+            "BufReadPre " .. vim.fn.expand("~") .. "/usr/Documents/Obsidian/Vaults/Notes/*.md",
+            "BufNewFile " .. vim.fn.expand("~") .. "/usr/Documents/Obsidian/Vaults/Notes/**/*.md",
+            "BufNewFile " .. vim.fn.expand("~") .. "/usr/Documents/Obsidian/Vaults/Notes/*.md",
+        },
         dependencies = {
-            -- Required.
             "nvim-lua/plenary.nvim",
-
-            -- see below for full list of optional dependencies 👇
+            "nvim-telescope/telescope.nvim",
+            "nvim-treesitter/nvim-treesitter"
         },
         opts = {
-            -- workspaces = {
-            -- 	{
-            -- 		name = "personal",
-            -- 		path = "~/vaults/personal",
-            -- 	},
-            -- 	{
-            -- 		name = "work",
-            -- 		path = "~/vaults/work",
-            -- 	},
-            -- },
-            --
-            -- see below for full list of options 👇
+            workspaces = {
+                {
+                    name = "Notes",
+                    path = "~/usr/Documents/Obsidian/Vaults/Notes"
+                },
+            },
+            completion = {
+                -- Set to false to disable completion.
+                nvim_cmp = true,
+                -- Trigger completion at 2 chars.
+                min_chars = 2,
+            },
+            preferred_link_style = "wiki",
+            disable_frontmatter = false,
+            follow_url_func = function(url)
+                vim.fn.jobstart({ "xdg-open", url }) -- linux
+            end,
+            picker = {
+                -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+                name = "telescope.nvim",
+                -- Optional, configure key mappings for the picker. These are the defaults.
+                -- Not all pickers support all mappings.
+                mappings = {
+                    -- Create a new note from your query.
+                    new = "<C-x>",
+                    -- Insert a link to the selected note.
+                    insert_link = "<C-l>",
+                },
+            },
+
+            sort_by = "modified",
+            sort_reversed = true,
+            open_notes_in = "current",
+            ui = {
+                enable = true,         -- set to false to disable all additional syntax features
+                update_debounce = 200, -- update delay after a text change (in milliseconds)
+                -- Define how various check-boxes are displayed
+                checkboxes = {
+                    -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+                    [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+                    ["x"] = { char = "", hl_group = "ObsidianDone" },
+                    [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+                    ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+                    -- Replace the above with this if you don't have a patched font:
+                    -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+                    -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+                    -- You can also add more custom ones...
+                },
+                -- Use bullet marks for non-checkbox lists.
+                bullets = { char = "•", hl_group = "ObsidianBullet" },
+                external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+                -- Replace the above with this if you don't have a patched font:
+                -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+                reference_text = { hl_group = "ObsidianRefText" },
+                highlight_text = { hl_group = "ObsidianHighlightText" },
+                tags = { hl_group = "ObsidianTag" },
+                hl_groups = {
+                    -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+                    ObsidianTodo = { bold = true, fg = "#f78c6c" },
+                    ObsidianDone = { bold = true, fg = "#89ddff" },
+                    ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+                    ObsidianTilde = { bold = true, fg = "#ff5370" },
+                    ObsidianBullet = { bold = true, fg = "#89ddff" },
+                    ObsidianRefText = { underline = true, fg = "#c792ea" },
+                    ObsidianExtLinkIcon = { fg = "#c792ea" },
+                    ObsidianTag = { italic = true, fg = "#89ddff" },
+                    ObsidianHighlightText = { bg = "#75662e" },
+                },
+            },
         },
     },
     {
