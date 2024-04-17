f240fd89c0d8570762a724cff0db4bb6c993ebf7
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-04-17 00:18:40 -0300

    Automatic commit @[17/04/2024 00:17:35.355723920]

--- system/zsh/init.sh
+++ system/zsh/init.sh
@@ -167,16 +167,16 @@ function project_search() {
     if [[ -a "$(command -v tmux)" ]]; then
         tmux_running=$(pgrep tmux)
         if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-            tmux new-session -s "${prName}" -c "${prPath}"
+            tmux new-session -s $prName -c $prPath
             exit 0
         fi
 
-        if ! tmux has-session -t="${prName}" 2> /dev/null; then
+        if ! tmux has-session -t=$prName 2> /dev/null; then
             otmx=$TMUX
             unset TMUX
-            tmux new-session -ds "${prName}" -c "${prPath}"
+            tmux new-session -ds $prName -c $prPath
         fi
-            tmux switch-client -t "${prName}"
+            tmux switch-client -t $prName
             export TMUX=$otmx
     else
         cd "${prPath}"
