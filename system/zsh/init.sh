96689799a60a9d4de5616aeb3fabe57a180309bb
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-04-17 00:20:14 -0300

    Automatic commit @[17/04/2024 00:19:10.347107961]

--- system/zsh/init.sh
+++ system/zsh/init.sh
@@ -165,23 +165,29 @@ function project_search() {
     echo $prName
 
     if [[ -a "$(command -v tmux)" ]]; then
-        tmux_running=$(pgrep tmux)
-        if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-            tmux new-session -s $prName -c $prPath
-            exit 0
-        fi
-
-        if ! tmux has-session -t=$prName 2> /dev/null; then
-            otmx=$TMUX
-            unset TMUX
-            tmux new-session -ds $prName -c $prPath
-        fi
-            tmux switch-client -t $prName
-            export TMUX=$otmx
+        echo "TMUX EXISTS"
     else
-        cd "${prPath}"
-        clear
+        echo "NO"
     fi
+
+    # if [[ -a "$(command -v tmux)" ]]; then
+    #     tmux_running=$(pgrep tmux)
+    #     if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    #         tmux new-session -s $prName -c $prPath
+    #         exit 0
+    #     fi
+    #
+    #     if ! tmux has-session -t=$prName 2> /dev/null; then
+    #         otmx=$TMUX
+    #         unset TMUX
+    #         tmux new-session -ds $prName -c $prPath
+    #     fi
+    #         tmux switch-client -t $prName
+    #         export TMUX=$otmx
+    # else
+    #     cd "${prPath}"
+    #     clear
+    # fi
 
     unset prName
     unset prPath
